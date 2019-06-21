b1aa99b66a1d1df26171c95f7b5617c54158ee4b
Your Name <azhukovski@renault-belarus.by>
2019-06-21 17:05:37 +0300

    Add ability to provide learning speed & cycles amoount via html inputs

--- neuron/event-handlers.js
+++ neuron/event-handlers.js
@@ -1,5 +1,13 @@
 document.getElementById('run-epoch').addEventListener('click', function () {
     let solidFill = document.getElementById('fill-type').checked;
-    learningExample.runEpoch(neuron, 10000, 0.001);
+    learningExample.runEpoch(neuron, amountOfLearningCycles, learningSpeed);
     learningExample.decorateMatrixByNeuron(neuron, solidFill);
+});
+
+document.getElementById('cycle-amount').addEventListener('input', function (e) {
+    amountOfLearningCycles = e.target.value;
+});
+
+document.getElementById('learning-speed').addEventListener('input', function (e) {
+    learningSpeed = e.target.value;
 });
--- neuron/index.js
+++ neuron/index.js
@@ -1,3 +1,5 @@
+let amountOfLearningCycles = 10000;
+let learningSpeed = 0.001;
 let learningExample = new NeuronLearning(10, 10);
 const amountOfInputs = learningExample.samples[0].inputValues.length + 1;
 let neuron = new Neuron(amountOfInputs, 0.3);
